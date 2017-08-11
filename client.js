ff6f64ad7bd80df39614421c6a29f3887e457e7f
Reid Simonton <rsimonton@leadmethod.com>
2017-08-11 09:14:56 -0700

    LEAD-1508 #comment Added ability to feature, broke coin config out into separate file

--- client.js
+++ client.js
@@ -1,65 +1,57 @@
 'use strict';
 
-var nodeRestClient = require('node-rest-client');
+const fs = require('fs');
+const path = require('path');
+const restClient = require('node-rest-client');
+
+const args = require('yargs').argv
 
 const debugging = true;
-const baseUrl = 'https://api.coinmarketcap.com/v1/ticker/';
 const indent = 4;	// space count
-const sep = "\n\n";
-const coins = [
-	'ethereum',
-	'nolimitcoin',
-	'tenx',
-	'nem',
-	'neo',
-	'ripple',
-	'faircoin',
-	'megacoin',
-	'thegcccoin',
-];
+const sep = "\n";
 
-var client = new nodeRestClient.Client(),
+const coinsFile = path.resolve(__dirname, "coins.json");
+const config = JSON.parse(fs.readFileSync(coinsFile, 'utf8'));
+const userApis = Object.keys(config);
+
+const apis = {
+	coinmarketcap: 'https://api.coinmarketcap.com/v1/ticker/'
+};
+
+var client = new restClient.Client(),
 	errors = [],
-	url;
+	coins,
+	url,
+	result;
 
 const debug = (msg) => {
 	debugging && console.log(msg);
 };
 
-// direct way 
-coins.forEach((name) => {
-	
-	url = baseUrl + name;
-	debug(url);
+console.log(args.filter ? "\n" + args.filter + ':' : '');
 
-	client.get(url, function (data, response) {
-		console.dir(data);
-		if(data.error) {
-			errors.push(name + ': ' + data.error);
-		}
-		else {
- 			// parsed response body as js object 
-    		console.log(name + ': ' + JSON.stringify(data, null, indent) + sep);
-    		// raw response 
-    		//console.log(response);
-    	}
-    });
+userApis.forEach(api => {
+
+	coins = config[api];
+
+	// direct way 
+	coins.forEach((name) => {
+		
+		url = apis[api] + name;
+
+		client.get(url, function (data, response) {
+			if(data.error) {
+				errors.push(name + ': ' + data.error);
+			}
+			else {
+				if(args.filter) {
+					console.log("\t" + name + ': ' + data[0][args.filter]);
+				}
+				else {
+	 				// parsed response body as js object 
+	    			console.log(name + ': ' + JSON.stringify(data, null, indent) + sep);
+	    		}
+	    	}
+	    });
+	});
 });
-
-console.dir(errors);
-if(0 < errors.length) {
-	console.log("Error:\n");
-	errors.forEach((e) => console.log("\t" + e + "\n"));
-}
-
-/*
-// registering remote methods 
-client.registerMethod("jsonMethod", "http://remote.site/rest/json/method", "GET");
-
-client.methods.jsonMethod(function (data, response) {
-    // parsed response body as js object 
-    console.log(data);
-    // raw response 
-    console.log(response);
-});
-*/
